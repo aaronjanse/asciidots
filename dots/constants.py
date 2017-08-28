dcaddf4a83f3530ccd70aac8d076abdfd120b3b7
ddorn <diego.dorn@free.fr>
2017-08-28 19:27:15 +1000

    New constants module for more clear and modifiable code
    There is a lot of things to add here of course, but I will do it while reading the code

---
+++ dots/constants.py
@@ -0,0 +1,12 @@
+"""
+Here are defined the constants of the language.
+You will alos find shortcuts for repetitive values in the code like UP and DOWN.
+"""
+
+# Defining directions
+UP = (0, 1)
+DOWN = (0, -1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+DIRECTIONS = (UP, RIGHT, DOWN, LEFT)
