63aae27545b85bc6daab4532612e17b00376ddcb
ddorn <diego.dorn@free.fr>
2017-08-28 20:41:22 +1000

    :warning: corrected value of UP and DOWN

--- dots/constants.py
+++ dots/constants.py
@@ -4,8 +4,8 @@ You will alos find shortcuts for repetitive values in the code like UP and DOWN.
 """
 
 # Defining directions
-UP = (0, 1)
-DOWN = (0, -1)
+UP = (0, -1)
+DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
