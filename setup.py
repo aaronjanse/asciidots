64ca78831fd6eb2cfee954b5152c9d3475ff2b31
aaronduino <gitduino@gmail.com>
2017-10-22 14:42:05 -0700

    fix pip installation errors

--- setup.py
+++ setup.py
@@ -10,27 +10,18 @@ import glob
 
 read_rst = lambda f: open(f, 'r').read()
 
-
 setup(
     name='asciidots',
-
-    version='1.0.7',
-
+    version='1.0.8',
     description='Interpreter for the AsciiDots esolang.',
     long_description=read_rst('README.rst'),
-
     url='https://github.com/aaronduino/asciidots',
-
     author='Aaron Janse',
     author_email='gitduino@gmail.com',
-
     license='APGL-v3.0',
-
     python_requires='>=3',
-
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-
         'Intended Audience :: Education',
         'Intended Audience :: Other Audience',
         'Intended Audience :: Science/Research',
@@ -38,31 +29,20 @@ setup(
         'Topic :: Other/Nonlisted Topic',
         'Topic :: Software Development :: Interpreters',
         'Operating System :: OS Independent',
-
         'License :: OSI Approved :: GNU Affero General Public License v3',
-
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
-    keywords='asciidots esolang esoteric ascii dataflow language programming fun',
-
+    keywords=
+    'asciidots esolang esoteric ascii dataflow language programming fun',
     packages=['dots'],
     scripts=['interpret.py'],
-
     package_data={'dots': glob.glob('dots/libs/*.dots')},
-
-    data_files=[
-        ('dots', glob.glob('dots/libs/*.dots'))
-    ],
-
     install_requires=['Click'],
-
     entry_points='''
         [console_scripts]
         asciidots=interpret:main
-    ''',
-)
+    ''', )
